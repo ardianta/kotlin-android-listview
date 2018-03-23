005ed25829aa5fed977d0290d8e7bfe87a9a84d8
Ardianta Pargo <ardianta_pargo@yahoo.co.id>
2018-03-24 01:56:13 +0800

    add file

---
+++ app/src/main/java/com/ardianta/kotlinlistview/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ardianta.kotlinlistview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var listView : ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listView = findViewById<ListView>(R.id.recipe_list_view)
+        val recipeList = Recipe.getRecipesFromFile("recipes.json", this)
+        val listItems = arrayOfNulls<String>(recipeList.size)
+
+        for(i in 0 until recipeList.size){
+            val recipe = recipeList[i]
+            listItems[i] = recipe.title
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+    }
+}
